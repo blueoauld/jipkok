b91ac4341df1e4a98b5935ea56b5c2b9eb717824
twobetta <sueh9898@gmail.com>
2026-08-05 00:52:26 +0900

    fix: Firebase 팟이 정적 라이브러리로 빌드되게 한다
    SPM을 끄고 필요한 팟에만 모듈 헤더를 켠다. 나머지 팟의 링크 방식은
    바꾸지 않는다.

---
+++ app/plugins/withFirebaseDisableSPM.js
@@ -0,0 +1,47 @@
+const { withDangerousMod } = require("expo/config-plugins");
+const fs = require("fs");
+const path = require("path");
+
+const FLAG = "$RNFirebaseDisableSPM = true";
+
+const MODULAR_HEADER_PODS = [
+  "GoogleUtilities",
+  "FirebaseCoreInternal",
+  "FirebaseCore",
+  "FirebaseInstallations",
+  "GoogleDataTransport",
+  "nanopb",
+];
+
+const TARGET_LINE = "target 'app' do";
+
+module.exports = function withFirebaseDisableSPM(config) {
+  return withDangerousMod(config, [
+    "ios",
+    (modConfig) => {
+      const podfile = path.join(
+        modConfig.modRequest.platformProjectRoot,
+        "Podfile",
+      );
+      let contents = fs.readFileSync(podfile, "utf8");
+
+      if (!contents.includes(FLAG)) {
+        contents = `${FLAG}\n\n${contents}`;
+      }
+
+      const pods = MODULAR_HEADER_PODS.filter(
+        (pod) => !contents.includes(`pod '${pod}'`),
+      )
+        .map((pod) => `  pod '${pod}', :modular_headers => true`)
+        .join("\n");
+
+      if (pods) {
+        contents = contents.replace(TARGET_LINE, `${TARGET_LINE}\n${pods}`);
+      }
+
+      fs.writeFileSync(podfile, contents);
+
+      return modConfig;
+    },
+  ]);
+};
