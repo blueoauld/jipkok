f55b36276e3671b848403eaed84ab3deee306d06
twobetta <sueh9898@gmail.com>
2026-08-04 17:45:52 +0900

    refactor: 스키마 변경 이력을 하나로 합친다

--- server/src/main/resources/db/migration/V2__soft_delete_partial_unique.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE member
-    DROP CONSTRAINT IF EXISTS member_phone_number_key;
-ALTER TABLE member
-    DROP CONSTRAINT IF EXISTS member_nickname_key;
-
-CREATE UNIQUE INDEX uk_member_phone_number ON member (phone_number) WHERE deleted_at IS NULL;
-CREATE UNIQUE INDEX uk_member_nickname ON member (nickname) WHERE deleted_at IS NULL;
-
-CREATE UNIQUE INDEX uk_chat_room_members ON chat_room (low_member_id, high_member_id) WHERE deleted_at IS NULL;
